18e875c5bf8ebf0cd2d1619af7261d803ed85228
Nahv23 <nereaparicio@gmail.com>
2020-10-12 23:12:55 +0200

    Created switch case for the differents options

--- src/Start.java
+++ src/Start.java
@@ -25,8 +25,41 @@ public class Start {
 				System.out.println(" 0. Salir");
 
 				System.out.print("\n\nIntroduzca una opción:");
-
 				optionSelected = reader.nextInt();
+
+				switch (optionSelected) {
+					case 1:
+						System.out.println("1");
+						break;
+					case 2:
+						System.out.println("2");
+						break;
+					case 3:
+						System.out.println("3");
+						break;
+					case 4:
+						System.out.println("4");
+						break;
+					case 5:
+						System.out.println("5");
+						break;
+					case 6:
+						System.out.println("6");
+						break;
+					case 7:
+						System.out.println("7");
+						break;
+					case 8:
+						System.out.println("8");
+						break;
+					case 0:
+						System.out.println("bye");
+						break;
+					default:
+						System.out.println("Por favor, selecciona una de las siguientes opciones");
+						break;
+				}
+
 			} while (optionSelected != 0);
 
 		} finally {
