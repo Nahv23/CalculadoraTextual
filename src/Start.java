0c8b8509d37880ac7249fa5ca7164d25add6a461
Nahv23 <nereaparicio@gmail.com>
2020-10-13 13:23:18 +0200

    Added suma function, refactored code and changed some messages

--- src/Start.java
+++ src/Start.java
@@ -1,35 +1,56 @@
 import java.util.Scanner;
 
-public class Start {
 
+public class Start {
+	static Scanner reader = new Scanner(System.in);
+	
+	public static void printMenu() {
+		System.out.println("\n_____________________________");
+		System.out.println("\nCalculadora de Calculator S.A");
+		System.out.println("_____________________________");
+
+		System.out.println("\n\nIntroduzca una opción del menu:");
+		System.out.println(" 1. Sumar ");
+		System.out.println(" 2. Restar ");
+		System.out.println(" 3. Multiplicar ");
+		System.out.println(" 4. Dividir ");
+		System.out.println(" 5. Resto (modulo) ");
+		System.out.println(" 6. Zoodíaco ");
+		System.out.println(" 7. Número mayor de 3 números ");
+		System.out.println(" 8. Capicua ");
+		System.out.println(" 0. Salir");		
+	}
+	
+	public static void suma() {
+		float num1;
+		float num2;
+		
+		System.out.print("\nIntroduzca el primer número flotante a sumar: ");
+		num1 = reader.nextFloat();
+		
+		System.out.print("Introduzca el segundo número flotante a sumar: ");
+		num2 = reader.nextFloat();
+		
+		System.out.println("La suma de "+ num1 +" y "+ num2 +" es "+ (num1+num2) );		
+	}
+	
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+
 
 		int optionSelected;
+		
+
 
 		try {
 			do {
-				System.out.println("_____________________________");
-				System.out.println("\nCalculadora de Calculator S.A");
-				System.out.println("_____________________________");
-
-				System.out.println("\n\nIntroduzca una opción del menu:");
-				System.out.println(" 1. Sumar ");
-				System.out.println(" 2. Restar ");
-				System.out.println(" 3. Multiplicar ");
-				System.out.println(" 4. Dividir ");
-				System.out.println(" 5. Resto (modulo) ");
-				System.out.println(" 6. Zoodíaco ");
-				System.out.println(" 7. Número mayor de 3 números ");
-				System.out.println(" 8. Capicua ");
-				System.out.println(" 0. Salir");
-
+				printMenu();
+				
 				System.out.print("\n\nIntroduzca una opción:");
 				optionSelected = reader.nextInt();
 
 				switch (optionSelected) {
 					case 1:
-						System.out.println("1");
+						suma();
 						break;
 					case 2:
 						System.out.println("2");
@@ -53,10 +74,10 @@ public class Start {
 						System.out.println("8");
 						break;
 					case 0:
-						System.out.println("bye");
+						System.out.println("El programa ha finalizado");
 						break;
 					default:
-						System.out.println("Por favor, selecciona una de las siguientes opciones");
+						System.out.println("Opción errónea”");
 						break;
 				}
 
